325c809aad295384108ed85dda17738dc330012b
isaacn123 <nsambai72@gmail.com>
2022-04-16 03:20:28 +0300

    edit theemes

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,7 +1,7 @@
 import AuthLogo from './extensions/logo_auth.png';
 import MenuLogo from './extensions/logo_menu.png';
 import favicon from './extensions/favicon.ico';
-import { synthwave } from './custom-theme';
+// import { synthwave } from './custom-theme';
 export default {
     config: {
         auth: {
@@ -14,31 +14,31 @@ export default {
             favicon: favicon,
           },
           theme: {
-              colors: synthwave,
-            // colors:{
-            //     danger100: '#403949', // TOGGLE OFF BG
-            //     danger700: '#c13c58', // TOGGLE OFF COLOR
-            //     neutral0: '#ffffff', // BACKGROUND SIDEBAR #241b2e
-            //     neutral100: '#f6f6f9', // BACKGROUND CONTENT
-            //     neutral150: '#ffffff', // DISABLED BUTTON BG #241b2e
-            //     neutral200: '#dcdce4', // LINE THROUGH MIDDLE
-            //     neutral500: '#8e8ea9', // MENU IDLE COLOR
-            //     neutral600: '#9a96a4', // MENU HOVER COLOR
-            //     neutral700: '#9a96a4', // MENU ACTIVE ICON COLOR
-            //     neutral800: '#32324d', // TITLE COLORS
-            //     neutral900: '#403949', // TOOLTIP BG
-            //     primary100: '#f0f0ff', // MENU ACTIVE BG
-            //     primary200: '#ffffff', // PLUS SIGN BG
-            //     primary500: '#7f7145', // PRIMARY BUTTON HOVER
-            //     primary600: '#bba551', // PRIMARY BUTTON COLOR
-            //     primary700: '#33adae', // HIGHLIGHT COLOR
-            //     secondary100: '#eafbe7', // INFO BOX COLOR
-            //     success100: '#eafbe7', // SUCCESS BOX BG
-            //     secondary200: '#ffd5b8',
-            //     secondary500: '#eda164',
-            //     secondary600: '#b0600b',
-            //     secondary700: '#965800',
-            // }
+            //   colors: synthwave,
+            colors:{
+                danger100: '#403949', // TOGGLE OFF BG
+                danger700: '#c13c58', // TOGGLE OFF COLOR
+                neutral0: '#ffffff', // BACKGROUND SIDEBAR #241b2e
+                neutral100: '#f6f6f9', // BACKGROUND CONTENT
+                neutral150: '#ffffff', // DISABLED BUTTON BG #241b2e
+                neutral200: '#dcdce4', // LINE THROUGH MIDDLE
+                neutral500: '#8e8ea9', // MENU IDLE COLOR
+                neutral600: '#9a96a4', // MENU HOVER COLOR
+                neutral700: '#9a96a4', // MENU ACTIVE ICON COLOR
+                neutral800: '#32324d', // TITLE COLORS
+                neutral900: '#403949', // TOOLTIP BG
+                primary100: '#f0f0ff', // MENU ACTIVE BG
+                primary200: '#ffffff', // PLUS SIGN BG
+                primary500: '#7f7145', // PRIMARY BUTTON HOVER
+                primary600: '#bba551', // PRIMARY BUTTON COLOR
+                primary700: '#33adae', // HIGHLIGHT COLOR
+                secondary100: '#eafbe7', // INFO BOX COLOR
+                success100: '#eafbe7', // SUCCESS BOX BG
+                secondary200: '#ffd5b8',
+                secondary500: '#eda164',
+                secondary600: '#b0600b',
+                secondary700: '#965800',
+            }
         },
         //   theme: {
         //     colors: {
