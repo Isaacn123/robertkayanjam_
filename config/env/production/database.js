9dea6ddd384449fad98e8bbffc365c8f12d6778b
isaacn123 <nsambai72@gmail.com>
2022-04-16 16:06:05 +0300

    update env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,11 +9,10 @@ const parse = require('pg-connection-string').parse;
           database: config.database,
           user: config.user,
           password: config.password,
-          ssl: env.bool('DATABASE_SSL', false),
-          // ssl: {
-          //   rejectUnauthorized: false,
+          ssl: {
+            rejectUnauthorized: false,
             
-          // },
+          },
         },
         debug: false,
       },
